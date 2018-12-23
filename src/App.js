b8c5703be1d0b0421656fcbb597809821c2b2e90
Christin Mikolai <mail@christinmikolai.de>
2018-12-23 06:38:23 +0100

    display SizePicker

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    height: 1,
+    width: 1,
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +17,13 @@ class App extends Component {
         <div className="App-Content">
           <div className="App-Settings">
             <h2>Choose Grid Size</h2>
+            <form id="sizePicker">
+              <p>Grid Height:
+              <input type="number" id="input_height" name="height" min="1" value={this.state.height} /></p>
+              <p>Grid Width:
+              <input type="number" id="input_width" name="width" min="1" value={this.state.width} /></p>
+              <p><input type="submit" id="input_submit" /></p>
+            </form>
 
             <hr className="Separator" />
 
