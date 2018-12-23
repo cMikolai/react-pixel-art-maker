97dad061c93db7bdd7add6512ee1d82db2a53610
Christin Mikolai <mail@christinmikolai.de>
2018-12-23 07:19:03 +0100

    fix input error/adds onChange

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {
-    height: 1,
-    width: 1,
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: 1,
+      width: 1,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,10 +34,15 @@ class App extends Component {
           <div className="App-Settings">
             <h2>Choose Grid Size</h2>
             <form id="sizePicker">
-              <p>Grid Height:
-              <input type="number" id="input_height" name="height" min="1" value={this.state.height} /></p>
-              <p>Grid Width:
-              <input type="number" id="input_width" name="width" min="1" value={this.state.width} /></p>
+              <label>
+              Grid Height:
+              <input type="number" id="input_height" name="height" min="1" value={this.state.height} onChange={this.handleChange} />
+              </label>
+              <br />
+              <label>
+              Grid Width:
+              <input type="number" id="input_width" name="width" min="1" value={this.state.width} onChange={this.handleChange} />
+              </label>
               <p><input type="submit" id="input_submit" /></p>
             </form>
 
