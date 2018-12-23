daf6f9b5b34c3bcd6a4aebe3f20f347b293c3d87
Christin Mikolai <mail@christinmikolai.de>
2018-12-23 06:19:58 +0100

    simple/basic App layout/structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Pixel Art Maker</h1>
         </header>
+
+        <div className="App-Content">
+          <div className="App-Settings">
+            <h2>Choose Grid Size</h2>
+
+            <hr className="Separator" />
+
+            <h2>Pick A Color</h2>
+            <input type="color" id="colorPicker" />
+          </div>
+
+          <div className="Canvas">
+            <h2>Design Canvas</h2>
+          </div>
+        </div>
+
       </div>
     );
   }
