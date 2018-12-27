d4aae50c67aaea4c939d73523c595c8317a97dbb
Christin Mikolai <mail@christinmikolai.de>
2018-12-27 10:16:29 +0100

    Updates

---
+++ precache-manifest.244abafb07ea897942d762ed9a272791.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2b86fffb62ab3eb28f7",
+    "url": "/react-pixel-art-maker/static/js/runtime~main.a2b86fff.js"
+  },
+  {
+    "revision": "1aa034f7a505d13f2bc9",
+    "url": "/react-pixel-art-maker/static/js/main.1aa034f7.chunk.js"
+  },
+  {
+    "revision": "0148014f8f40fab6133c",
+    "url": "/react-pixel-art-maker/static/js/1.0148014f.chunk.js"
+  },
+  {
+    "revision": "1aa034f7a505d13f2bc9",
+    "url": "/react-pixel-art-maker/static/css/main.9db41d00.chunk.css"
+  },
+  {
+    "revision": "07f6deee85632f61e85dc58106965ba9",
+    "url": "/react-pixel-art-maker/index.html"
+  }
+];
